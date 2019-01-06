43e54249c24d983c2d3da714650f96247b92f13c
takeyuichi <y.takey@gmail.com>
2019-01-06 20:44:26 +0900

    convert to typescript

--- src/Fireworks.tsx
+++ src/Fireworks.tsx
@@ -1,13 +1,37 @@
 import * as React from "react";
 
+type Props = {
+  width?: number;
+  height?: number;
+  maxFireworks?: number;
+  maxSparks?: number;
+};
+
+type Spark = {
+  vx: number;
+  vy: number;
+  weight: number;
+  red: number;
+  green: number;
+  blue: number;
+};
+
+type Firework = {
+  x: number;
+  y: number;
+  age: number;
+  phase: "fly" | "explode";
+  sparks: Array<Spark>;
+};
+
 const radius = 4;
 
-const times = (n, func) =>
+const times = (n: number, func: Function) =>
   Array(n)
     .fill(null)
     .map((_, i) => func(i));
 
-const initFirework = (width, height) => {
+const initFirework = (width: number, height: number) => {
   return {
     x: Math.floor(Math.random() * width),
     y: height,
@@ -16,8 +40,8 @@ const initFirework = (width, height) => {
   };
 };
 
-const generateSparks = maxSparks => {
-  return times(maxSparks, i => {
+const generateSparks = (maxSparks: number) => {
+  return times(maxSparks, (i: number) => {
     const vx = Math.random() * 5 + 0.5;
     const vy = Math.random() * 5 + 0.5;
 
@@ -32,8 +56,13 @@ const generateSparks = maxSparks => {
   });
 };
 
-const generateFireworks = (width, height, maxFireworks, maxSparks) => {
-  return times(maxFireworks, i => {
+const generateFireworks = (
+  width: number,
+  height: number,
+  maxFireworks: number,
+  maxSparks: number,
+) => {
+  return times(maxFireworks, (i: number) => {
     return {
       ...initFirework(width, height),
       sparks: generateSparks(maxSparks),
@@ -41,37 +70,38 @@ const generateFireworks = (width, height, maxFireworks, maxSparks) => {
   });
 };
 
-const toRGBA = (r, g, b = 0, a = 1) => `rgba(${[r, g, b, a].join(",")})`;
+const toRGBA = (r = 0, g = 0, b = 0, a = 1) => `rgba(${[r, g, b, a].join(",")})`;
 
-const fill = (ctx, x, y, colors) => {
+const fill = (ctx: any, x: number, y: number, colors: Array<number>) => {
   ctx.beginPath();
   ctx.fillStyle = toRGBA(...colors);
   ctx.rect(x, y, radius, radius);
   ctx.fill();
 };
 
-const draw = (ctx, { phase, sparks, age, x, y }, index) => {
-  if (phase === "explode") {
-    sparks.forEach(spark => {
-      times(10, i => {
-        const trailAge = age + i;
-        const fade = i * 20 - age * 2;
-        fill(
-          ctx,
-          x + spark.vx * trailAge,
-          y + spark.vy * trailAge + (spark.weight * trailAge) ** 2,
-          ["red", "green", "blue"].map(key => Math.floor(spark[key] * fade)),
-        );
-      });
-    });
-  } else {
-    times(15, i => {
+const draw = (ctx: any, { phase, sparks, age, x, y }: Firework, index: number) => {
+  if (phase === "fly") {
+    times(15, (i: number) => {
       fill(ctx, x + Math.random() * i - i / 2, y + i * radius, [index * 50, i * 17]);
     });
+    return;
   }
+
+  sparks.forEach(spark => {
+    times(10, (i: number) => {
+      const trailAge = age + i;
+      const fade = i * 20 - age * 2;
+      fill(
+        ctx,
+        x + spark.vx * trailAge,
+        y + spark.vy * trailAge + (spark.weight * trailAge) ** 2,
+        ["red", "green", "blue"].map(key => Math.floor(spark[key] * fade)),
+      );
+    });
+  });
 };
 
-const update = (width, height, firework) => {
+const update = (width: number, height: number, firework: Firework) => {
   if (firework.phase === "explode") {
     const others = firework.age > 100 && Math.random() < 0.05 ? initFirework(width, height) : {};
     return { ...firework, age: firework.age + 1, ...others };
@@ -81,12 +111,8 @@ const update = (width, height, firework) => {
   }
 };
 
-const Fireworks = () => {
+const Fireworks = ({ width, height, maxFireworks, maxSparks }: Props) => {
   const canvas = React.useRef(null);
-  const width = 800;
-  const height = 500;
-  const maxFireworks = 5;
-  const maxSparks = 50;
   const [fireworks, updator] = React.useState(
     generateFireworks(width, height, maxFireworks, maxSparks),
   );
@@ -95,7 +121,7 @@ const Fireworks = () => {
     const context = canvas.current.getContext("2d");
     context.clearRect(0, 0, width, height);
     updator(
-      fireworks.map((firework, index) => {
+      fireworks.map((firework: Firework, index: number) => {
         draw(context, firework, index);
         return update(width, height, firework);
       }),
@@ -103,6 +129,13 @@ const Fireworks = () => {
   });
 
   return <canvas ref={canvas} width={width} height={height} />;
+};
+
+Fireworks.defaultProps = {
+  width: 800,
+  height: 500,
+  maxFireworks: 5,
+  maxSparks: 50,
 };
 
 export default Fireworks;
